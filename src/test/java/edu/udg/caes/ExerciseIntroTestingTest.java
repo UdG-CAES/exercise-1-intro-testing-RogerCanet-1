733c2b362b22d6b562bd20ef9eb6f0d745842c89
IKanet <Canet979>
2020-10-05 17:20:35 +0200

    Afegits tests funcionals

--- src/test/java/edu/udg/caes/ExerciseIntroTestingTest.java
+++ src/test/java/edu/udg/caes/ExerciseIntroTestingTest.java
@@ -29,4 +29,28 @@ public class ExerciseIntroTestingTest {
         final int result = ExerciseIntroTesting.oddOrPos(new int []{-3, -2, 0, 1, 4});
         assertEquals(result, 3);
     }
+
+    @Test
+    public void NOUtestFindLast(){
+        final int result = ExerciseIntroTesting.findLast(new int []{2, 3, 5, 6}, 3);
+        assertEquals(result, 1);
+    }
+
+    @Test
+    public void NOUtestLastZero(){
+        final int result = ExerciseIntroTesting.lastZero(new int []{3, 1, 0});
+        assertEquals(result, 2);
+    }
+
+    @Test
+    public void NOUtestCountPositive(){
+        final int result = ExerciseIntroTesting.countPositive(new int []{-4, 2, 2});
+        assertEquals(result, 2);
+    }
+
+    @Test
+    public void NOUtestOddOrPos(){
+        final int result = ExerciseIntroTesting.oddOrPos(new int []{-3, -2, 5, 1, 4});
+        assertEquals(result, 3);
+    }
 }
